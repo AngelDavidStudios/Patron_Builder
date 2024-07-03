91363bcff55d428d6e8927b75cb84bc4d1eb8819
Angel David <angeldavidstudios@outlook.com>
2024-07-03 18:04:36 -0500

    Inicio del proyecto Patron Builder

---
+++ Builder_Pattern_Pizzeria/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Builder_Pattern_Pizzeria;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ Builder_Pattern_Pizzeria/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Builder_Pattern_Pizzeria;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
